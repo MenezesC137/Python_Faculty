851a64d46d1fc72681aa79499acef74afc0740c0
MenezesC137 <iamcarloseduardo@hotmail.com>
2023-08-24 19:58:09 -0300

    feat: final de semana

---
+++ Basic_Python/Aula 24.08/verificar_final_de_semana.py
@@ -0,0 +1,5 @@
+dia = input("Digite um dia da semana: ")
+if dia == "sábado" or dia == "domingo" or dia == "Sábado" or dia == "Domingo" or dia == "SABADO" or dia == "DOMINGO" or dia == "sabado" or dia == "domingo":
+    print("Fim de semana")
+else:
+    print("Dia de semana")
