2c9ee568569456ce353d2d5ed02d2bc0e21b12b8
MenezesC137 <iamcarloseduardo@hotmail.com>
2023-08-24 19:54:25 -0300

    feat: confirma senha

---
+++ Basic_Python/Aula 24.08/confirmar_senha.py
@@ -0,0 +1,7 @@
+senha = input("Digite uma senha: ")
+confirmar_senha = input("Confirme sua senha: ")
+
+if senha == confirmar_senha:
+    print("Senha confirmada!")
+else:
+    print("Senha incorreta, tente novamente.")
